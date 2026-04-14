f219e2a4991809b5f006f84f7d9441eb53744973
allsvenskanai <aisvenskan88@gmail.com>
2026-04-14 22:23:33 +0200

    Create admin.js

---
+++ src/views/admin.js
@@ -0,0 +1,5 @@
+import { emptyState } from '../components/loading-state.js';
+
+export async function renderAdmin(){
+  return emptyState('Admin UI kommer i Fas 2', 'Backend-actions finns kvar i /api/admin. Ny adminpanel byggs ovanpå samma actionmodell.');
+}
